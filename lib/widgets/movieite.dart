f641d79f5c76b44801ab47f339fb44b8212bdeee
tejaschinni <tejaschinni31@gmail.com>
2023-04-24 09:50:06 +0530

    update

--- lib/widgets/movieite.dart
+++ lib/widgets/movieite.dart
@@ -9,82 +9,89 @@ class MovieItem extends StatelessWidget {
   Widget build(BuildContext context) {
     DateTime date = DateTime.fromMillisecondsSinceEpoch(
         int.parse(result.releasedDate.toString()));
-    return Column(
-      children: [
-        Row(
-          children: [
-            Column(
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.15,
+            child: Row(
               children: [
-                const Icon(
-                  Icons.arrow_drop_up,
-                  size: 40,
+                Column(
+                  children: [
+                    const Icon(
+                      Icons.arrow_drop_up,
+                      size: 40,
+                    ),
+                    Text(result.voting.toString()),
+                    const Icon(
+                      Icons.arrow_drop_down_sharp,
+                      size: 40,
+                    ),
+                    const Text("Votes")
+                  ],
                 ),
-                Text(result.voting.toString()),
-                const Icon(
-                  Icons.arrow_drop_down_sharp,
-                  size: 40,
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.24,
+                  width: MediaQuery.of(context).size.width * 0.24,
+                  child: Image.network(result.poster.toString()),
                 ),
-                const Text("Votes")
+                Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 200,
+                        child: Text(
+                          result.title,
+                          style:
+                              const TextStyle(fontFamily: 'LibreBaskerville'),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 200,
+                        child: Text("Genre : ${result.genre.toString()}",
+                            overflow: TextOverflow.ellipsis),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        child: Text("Director : ${result.director[0]}",
+                            overflow: TextOverflow.ellipsis),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.55,
+                        child: Text(
+                            "Starring : ${result.stars.toString().replaceAll('[', '').replaceAll(']', "")}",
+                            overflow: TextOverflow.ellipsis),
+                      ),
+                      SizedBox(
+                        width: 200,
+                        child: Text(
+                            "Mins | ${result.language} |${date.day} ${date.month}"),
+                      ),
+                      SizedBox(
+                        width: 200,
+                        child: Text(
+                            "${result.pageViews} views | Voted by ${result.totalVoted} People"),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.24,
-              width: MediaQuery.of(context).size.width * 0.24,
-              child: Image.network(result.poster.toString()),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 200,
-                    child: Text(
-                      result.title,
-                      style: const TextStyle(fontFamily: 'LibreBaskerville'),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 200,
-                    child: Text("Genre : ${result.genre.toString()}",
-                        overflow: TextOverflow.ellipsis),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: Text("Director : ${result.director[0]}",
-                        overflow: TextOverflow.ellipsis),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.55,
-                    child: Text(
-                        "Starring : ${result.stars.toString().replaceAll('[', '').replaceAll(']', "")}",
-                        overflow: TextOverflow.ellipsis),
-                  ),
-                  SizedBox(
-                    width: 200,
-                    child: Text(
-                        "Mins | ${result.language} |${date.day} ${date.month}"),
-                  ),
-                  SizedBox(
-                    width: 200,
-                    child: Text(
-                        "${result.pageViews} views | Voted by ${result.totalVoted} People"),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-                onPressed: () {}, child: const Text("Watch Trailer")),
           ),
-        )
-      ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {}, child: const Text("Watch Trailer")),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
